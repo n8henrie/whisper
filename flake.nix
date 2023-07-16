86ae93c3b5b7f89b5a5cfc112fce17e35143a9ee
Nathan Henrie <nate@n8henrie.com>
2023-07-16 16:10:41 -0600

    Make easier to support other systems

--- flake.nix
+++ flake.nix
@@ -3,25 +3,46 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
 
-  outputs = {nixpkgs, ...}: let
-    system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages.${system}.default = with pkgs.python310Packages;
-      buildPythonPackage {
-        name = "whisper";
-        version = "20230314";
-        format = "setuptools";
-        src = ./.;
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    systems = ["aarch64-darwin"];
+  in
+    builtins.foldl' (acc: system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      lib.recursiveUpdate acc
+      {
+        packages.${system} = {
+          default = self.packages.${system}.whisper;
+          whisper = with pkgs.python310Packages;
+            buildPythonPackage {
+              name = "whisper";
+              version = "20230314";
+              format = "setuptools";
+              src = ./.;
 
-        propagatedBuildInputs = [
-          numba
-          numpy
-          torch
-          tqdm
-          more-itertools
-          tiktoken
-        ];
-      };
-  };
+              doCheck = false;
+              propagatedBuildInputs = [
+                numba
+                numpy
+                torch
+                tqdm
+                more-itertools
+                tiktoken
+              ];
+            };
+        };
+        apps.${system} = {
+          default = self.apps.${system}.whisper;
+          whisper = {
+            type = "app";
+            program = "${self.packages.${system}.whisper}/bin/whisper";
+          };
+        };
+      }) {}
+    systems;
 }
